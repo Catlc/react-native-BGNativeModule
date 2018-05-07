cb4c51ca2a30e80bbbd4fe3266f4f35b85f98880
李承 <165205770@qq.com>
2018-05-07 13:29:35 +0800

    add Project

---
+++ android/src/main/java/com/example/nativemoduleexample/IWatermarkInterface.java
@@ -0,0 +1,10 @@
+package com.example.nativemoduleexample;
+
+
+/**
+ * Created by ggx
+ */
+
+public interface IWatermarkInterface {
+    void setWatermark(String label, int degress, int fontSize, String color, int horizentalSpace, int verSpace);
+}
---
+++ ios/BGNativeModuleExample/BGNativeModuleExample.h
@@ -0,0 +1,17 @@
+//
+//  BGNativeModuleExample.h
+//  BGNativeModuleExample
+//
+//  Created by 李承 on 2018/4/23.
+//  Copyright © 2018年 licheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
+
+@interface BGNativeModuleExample : NSObject<RCTBridgeModule>
+
+@end
